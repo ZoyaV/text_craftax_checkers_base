159b98db35c5071ac0964e653edb5737aa1b10a9
zoya <zoya.v@ya.ru>
2024-08-07 15:32:39 +0300

    add setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='craftax_checkers',
+    version='0.1',
+    packages=find_packages(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ZoyaV/text_craftax_checkers_base',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
