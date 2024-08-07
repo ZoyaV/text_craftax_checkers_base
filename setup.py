d887cb6f9a5aa079da7b97a5b5b4c9a1ac2dd44f
zoya <zoya.v@ya.ru>
2024-08-07 15:34:26 +0300

    remove README from description

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     name='craftax_checkers',
     version='0.1',
     packages=find_packages(),
-    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/ZoyaV/text_craftax_checkers_base',
     classifiers=[
